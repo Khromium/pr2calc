b114be913a0b4de648f925bb65e94fef6a6f48ed
Khrom <devs@outlook.jp>
2015-12-02 00:31:40 +0900

    add: kadai5

--- pr2Calc/src/pr2Calc/SimultaneousEquation.java
+++ pr2Calc/src/pr2Calc/SimultaneousEquation.java
@@ -10,35 +10,55 @@ public class SimultaneousEquation extends Matrix {
         super(input);
     }
 
-    public double[] getAnswers_() {
-        return answers_;
-    }
 
     protected void _normalize(int targetRow) {
+        final double normal = _getComponentOf(targetRow, targetRow);
         for (int i = targetRow; i < _getNumOfColumn(); i++) {
-            m_[targetRow][i] = _getComponentOf(targetRow, i) / _getComponentOf(targetRow, targetRow);
+            m_[targetRow][i] /= normal;
         }
     }
 
     protected void _subtractRowFrom(int targetRow, int toRow) {
+        double divide = _getComponentOf(toRow, targetRow) / _getComponentOf(targetRow, targetRow);
         for (int i = 0; i < _getNumOfColumn(); i++) {
-            m_[toRow][i] = _getComponentOf(toRow, i) - _getComponentOf(targetRow, i) * _getComponentOf(toRow, i) / _getComponentOf(targetRow, i);
+            m_[toRow][i] -= divide * _getComponentOf(targetRow, i);
         }
     }
 
-
-    public static void main(String[] args) {
-        double[][] func = {{1, 2, 3, 4, 22}, {-3, 3, -2, 2, -14}, {6, -2, 4, -8, 8}, {3, -5, 1, 1, 23}};
-        SimultaneousEquation simultaneousEquation = new SimultaneousEquation(func);
-        simultaneousEquation._printMatrix();
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i + 1) + "行1列目が1となるように割り、他の行の" + (i + 1) + "列目が0となるように引く");
-            simultaneousEquation._normalize(i);
-            for (int j = i + 1; j < simultaneousEquation._getNumOfColumn() - 1; j++) {
-                simultaneousEquation._subtractRowFrom(i, j);
+    /**
+     * ガウスジョルダンの消去法を使って計算するメソッド
+     */
+    public void _solveByGaussJordan() {
+        answers_ = new double[_getNumOfRow()];
+        _printMatrix();
+        for (int i = 0; i < _getNumOfRow(); i++) {
+            System.out.println((i + 1) + "行" + (i + 1) + "列目が1となるように割り、他の行の" + (i + 1) + "列目が0となるように引く");
+            _normalize(i);
+            for (int j = 0; j < _getNumOfRow(); j++) {
+                if (i != j) _subtractRowFrom(i, j);
             }
-            simultaneousEquation._printMatrix();
+            _printMatrix();
         }
 
+        for (int i = 0; i < _getNumOfRow(); i++) {
+            answers_[i] = _getComponentOf(i, _getNumOfColumn() - 1);
+        }
+        System.out.println("Answer:");
+        for (int i = 0; i < _getNumOfRow(); i++) {
+            if (i != 0) System.out.print(", ");
+            System.out.print("x" + (i + 1) + "=" + String.format("%1$4.1f", answers_[i]));
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        double[][] func = {
+                {2, 1, 3, 4, 2},
+                {3, 2, 5, 2, 12},
+                {3, 4, 1, -1, 4},
+                {-1, -3, 1, 3, -1}};
+        SimultaneousEquation simultaneousEquation = new SimultaneousEquation(func);
+        simultaneousEquation._solveByGaussJordan();
     }
 }
