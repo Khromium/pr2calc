b49bbb98b85e74a57283c390c7315812eef9f19a
Khrom <devs@outlook.jp>
2015-12-09 13:20:11 +0900

    add: 完全選択法

--- pr2Calc/src/pr2Calc/SimultaneousEquation.java
+++ pr2Calc/src/pr2Calc/SimultaneousEquation.java
@@ -100,6 +100,42 @@ public class SimultaneousEquation extends Matrix {
         _printAnswer();
     }
 
+    protected void _solveByGaussWithCompleteSelection() {
+        answers_ = new double[_getNumOfRow()];
+        _printMatrix();
+        System.out.println();
+
+        int[] oIndex = new int[_getNumOfColumn() - 1];
+        for (int i = 0; i < _getNumOfColumn() - 1; i++) oIndex[i] = i;
+
+
+        //前進
+        for (int i = 0; i < _getNumOfRow() - 1; i++) {
+            int[] maxRow = _selectPivotFromRowColumn(i, i);
+            _exchangeRowsAndColumns(i, i, maxRow[0], maxRow[1]);
+
+            int tmpd = oIndex[i];
+            oIndex[i] = oIndex[maxRow[1]];
+            oIndex[maxRow[1]] = tmpd;
+
+            for (int j = i + 1; j < _getNumOfRow(); j++) {
+                _subtractRowFrom(i, j);
+            }
+            _printMatrix();
+            System.out.println();
+        }
+
+        //後退
+        for (int i = _getNumOfRow() - 1; i >= 0; i--) {
+            double anst = _getComponentOf(i, _getNumOfColumn() - 1);
+            for (int j = i + 1; j < _getNumOfRow(); j++) {
+                anst -= _getComponentOf(i, j) * answers_[oIndex[j]];
+            }
+            answers_[oIndex[i]] = anst / _getComponentOf(i, i);
+        }
+        _printAnswer();
+    }
+
     protected void _exchangeRows(int row1, int row2) {
         for (int i = 0; i < _getNumOfColumn(); i++) {
             double tmp = _getComponentOf(row1, i);
@@ -108,12 +144,21 @@ public class SimultaneousEquation extends Matrix {
         }
     }
 
-    protected int _selectPivotFromRow(int rowfrom, int column) /*throws invalidMatrixException*/ {
-//        if (rowfrom < _getNumOfRow()) {
-//            System.out.println("値が不正です");
-//            System.exit(1);
-////            new invalidMatrixException("値が不正です");
-//        }
+    protected void _exchangeColumns(int col1, int col2) {
+        for (int i = 0; i < _getNumOfRow(); i++) {
+            double tmp = _getComponentOf(i, col1);
+            _setComponentOf(i, col1, _getComponentOf(i, col2));
+            _setComponentOf(i, col2, tmp);
+        }
+    }
+
+    protected void _exchangeRowsAndColumns(int row1, int col1, int row2, int col2) {
+        _exchangeRows(row1, row2);
+        _exchangeColumns(col1, col2);
+    }
+
+
+    protected int _selectPivotFromRow(int rowfrom, int column) {
         int resultIndex = rowfrom;
         double maxvalue = Math.abs(_getComponentOf(rowfrom, column));
         for (int i = rowfrom + 1; i < _getNumOfRow(); i++) {
@@ -124,6 +169,29 @@ public class SimultaneousEquation extends Matrix {
             }
         }
         return resultIndex;
+    }
+
+    protected int _selectPivotFromRow(final int column) {
+        return _selectPivotFromRow(column, 0);
+    }
+
+    protected int[] _selectPivotFromRowColumn(int rowFrom, int colFrom) {
+        int[] maxIndex = new int[2];
+        maxIndex[0] = rowFrom;
+        maxIndex[1] = colFrom;
+        //初期値
+        double maxValue = Math.abs(_getComponentOf(rowFrom, colFrom));
+        for (int i = rowFrom; i < _getNumOfRow(); i++) {
+            for (int j = colFrom; j < _getNumOfColumn() - 1; j++) {
+                double tar = Math.abs(_getComponentOf(i, j));
+                if (maxValue < tar) {
+                    maxValue = tar;
+                    maxIndex[0] = i;
+                    maxIndex[1] = j;
+                }
+            }
+        }
+        return maxIndex;
     }
 
 
@@ -145,13 +213,12 @@ public class SimultaneousEquation extends Matrix {
 
 
         SimultaneousEquation simultaneousEquation = new SimultaneousEquation(func);
+        System.out.println("部分選択法");
         simultaneousEquation._solveByGaussWithPartialSelection();
+        System.out.println("\n完全選択法");
+        simultaneousEquation = new SimultaneousEquation(func);
+        simultaneousEquation._solveByGaussWithCompleteSelection();
     }
 
-//    class invalidMatrixException extends Exception {
-//        public invalidMatrixException(String args) {
-//            super(args);
-//        }
-//    }
 
 }
